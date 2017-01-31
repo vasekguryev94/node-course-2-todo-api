cf068c1f48d43848e0e3c04f4dfefb4c0f3c3d48
vasekguryev <guryevi30041994@gmail.com>
2017-01-31 19:08:52 +0200

    Add POST /users route

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,42 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// {
+//   email: 'guryevi30041994@gmail.com',
+//   password: 'sfdasgsdfghdsfhdhgfdfgh',
+//   tokens: [{
+//     access: 'auth',
+//     token: 'rtgretghrthrthrthgfdhrehrehnbdn'
+//   }]
+// }
 
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User};
