05a2b02a349ea289c3589c6e2f9656e561df9724
vasekguryev <guryevi30041994@gmail.com>
2017-02-01 17:45:14 +0200

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,42 +1,56 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// {
-//   email: 'guryevi30041994@gmail.com',
-//   password: 'sfdasgsdfghdsfhdhgfdfgh',
-//   tokens: [{
-//     access: 'auth',
-//     token: 'rtgretghrthrthrthgfdhrehrehnbdn'
-//   }]
-// }
-
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
-    }
-  },
-  password: {
-    type: String,
-    require: true,
-    minlength: 6
-  },
-  tokens: [{
-    access: {
+var UserSchema = new mongoose.Schema({
+    email: {
       type: String,
-      required: true
+      required: true,
+      trim: true,
+      minlength: 1,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: '{VALUE} is not a valid email'
+      }
     },
-    token: {
+    password: {
       type: String,
-      required: true
-    }
-  }]
+      require: true,
+      minlength: 6
+    },
+    tokens: [{
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
