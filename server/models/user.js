00f2f60ab1faa7564b53168fdc81f54788b6fb49
vasekguryev <guryevi30041994@gmail.com>
2017-02-02 18:18:54 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -53,7 +53,7 @@ UserSchema.methods.generateAuthToken = function () {
 };
 
 UserSchema.statics.findByToken = function (token) {
-  var user = token;
+  var User = this;
   var decoded;
 
   try {
@@ -66,6 +66,26 @@ UserSchema.statics.findByToken = function (token) {
     '_id' : decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
+  });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
   });
 };
 
